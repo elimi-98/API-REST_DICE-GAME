6ae622f4387160981ed6a111da041f0d5b56ee67
eli-mi98 <elisiavmi@gmail.com>
2023-10-02 19:22:17 +0200

    migrations, game model, controller user, controller game

---
+++ app/Models/Game.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'firt_dice',
+        'second_dice',
+        'game_won',
+    ];
+
+    public function user(){
+        return  $this->belongsTo(User::class);
+      }
+}
