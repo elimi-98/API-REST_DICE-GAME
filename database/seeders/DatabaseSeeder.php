09d6441ec4ce0748643fa24b39985d1a12c8c16d
eli-mi98 <elisiavmi@gmail.com>
2023-10-14 15:26:03 +0200

    UserController- logout done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -35,7 +35,7 @@ class DatabaseSeeder extends Seeder
         'password' => bcrypt(123456789), // password
         'remember_token' => Str::random(10),
 
-     ])->assignRole('player');
+        ])->assignRole('player');
 
 
     }
